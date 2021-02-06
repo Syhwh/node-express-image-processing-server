16fa42505857b0d308b570778abd99ad6f44fd44
Shymmy Garcia <shymmy.garcia-ext@block.one>
2021-02-06 10:47:24 -0500

    four module

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor')
 const router = Router();
 
 const filename = (request, file, callback) => {
@@ -25,8 +26,15 @@ const fileFilter = (request, file, callback) => {
 
 const upload = multer({ fileFilter, storage });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'),async (request, response) => {
 	if (request.fileValidationError) return response.status(400).json({ error: request.fileValidationError });
+	try {
+		await imageProcessor(request.file.filename)
+	} catch (error) {
+
+	}
+
+
 	return response.status(201).json({ success: true });
 });
 
